590f37837d0a0a41ba7a6d27541729a941255efb
Karthick <karthickk1996@hotmail.com>
2026-01-14 22:31:49 +0530

    fix: use different button variants with items-stretch for equal height

--- components/server/settings/Packages.vue
+++ components/server/settings/Packages.vue
@@ -248,12 +248,12 @@ onMounted(fetchComposerConfig)
           </div>
         </div>
 
-        <div class="mt-6 flex items-center gap-3">
+        <div class="mt-6 flex items-stretch gap-3">
           <Button variant="outline" @click="addCredential">
             <Icon name="lucide:plus" class="mr-2 h-4 w-4" />
             Add Credential
           </Button>
-          <Button variant="outline" :disabled="isSaving" @click="saveConfig">
+          <Button :disabled="isSaving" @click="saveConfig">
             <Icon v-if="isSaving" name="lucide:loader-2" class="mr-2 h-4 w-4 animate-spin" />
             <Icon v-else name="lucide:save" class="mr-2 h-4 w-4" />
             Save Configuration
