cb1f56ca38f9f4fd5ad385665aca9a476ba618ac
kkz6 <karthickk1996@hotmail.com>
2026-07-27 14:31:45 +0530

    Add active actions menu

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1805,6 +1805,7 @@ onMounted(fetchTeams);
       </div>
 
       <div class="flex items-center space-x-2">
+        <LayoutActiveActions />
         <!-- User Menu (with Teams) -->
         <ClientOnly>
           <DropdownMenu v-model:open="isOpen">
