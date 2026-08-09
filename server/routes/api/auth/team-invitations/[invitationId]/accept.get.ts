9e2bd10e711f64bb83e76c3f65ebbd32cd8c7a12
kkz6 <karthickk1996@hotmail.com>
2026-08-09 12:34:56 +0530

    Preserve invitation redirects

--- server/routes/api/auth/team-invitations/[invitationId]/accept.get.ts
+++ server/routes/api/auth/team-invitations/[invitationId]/accept.get.ts
@@ -4,5 +4,7 @@ export default defineEventHandler(async (event) => {
   const query = getRequestURL(event).search;
   const target = `${backendBase.replace(/\/$/, "")}/auth/team-invitations/${invitationId}/accept${query}`;
 
-  return proxyRequest(event, target);
+  return proxyRequest(event, target, {
+    fetchOptions: { redirect: "manual" },
+  });
 });
